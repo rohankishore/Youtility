80d13d35ac71bdef25e25ba1fa371d3103268c6c
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-03-03 00:32:53 +0530

    Create main.py

---
+++ youtility/main.py
@@ -0,0 +1,213 @@
+# coding:utf-8
+import sys
+
+import qdarktheme
+from PyQt6.QtCore import Qt, pyqtSignal, QEasingCurve, QUrl
+from PyQt6.QtGui import QIcon, QDesktopServices
+from PyQt6.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout, QApplication, QFrame
+from qfluentwidgets import (NavigationBar, NavigationItemPosition, MessageBox,
+                            isDarkTheme, setTheme, Theme,
+                            PopUpAniStackedWidget, setThemeColor)
+from qfluentwidgets import FluentIcon as FIF
+from qframelesswindow import FramelessWindow, TitleBar
+from downloader import YoutubeVideo
+from settings import SettingsPage
+
+
+class StackedWidget(QFrame):
+    """ Stacked widget """
+
+    currentChanged = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.hBoxLayout = QHBoxLayout(self)
+        self.view = PopUpAniStackedWidget(self)
+
+        self.hBoxLayout.setContentsMargins(0, 0, 0, 0)
+        self.hBoxLayout.addWidget(self.view)
+
+        self.view.currentChanged.connect(self.currentChanged)
+
+    def addWidget(self, widget):
+        """ add widget to view """
+        self.view.addWidget(widget)
+
+    def widget(self, index: int):
+        return self.view.widget(index)
+
+    def setCurrentWidget(self, widget, popOut=False):
+        if not popOut:
+            self.view.setCurrentWidget(widget, duration=300)
+        else:
+            self.view.setCurrentWidget(
+                widget, True, False, 200, QEasingCurve.Type.InQuad)
+
+    def setCurrentIndex(self, index, popOut=False):
+        self.setCurrentWidget(self.view.widget(index), popOut)
+
+
+class CustomTitleBar(TitleBar):
+    """ Title bar with icon and title """
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setFixedHeight(48)
+        self.hBoxLayout.removeWidget(self.minBtn)
+        self.hBoxLayout.removeWidget(self.maxBtn)
+        self.hBoxLayout.removeWidget(self.closeBtn)
+
+        # add window icon
+        self.iconLabel = QLabel(self)
+        self.iconLabel.setFixedSize(20, 20)
+        self.hBoxLayout.insertSpacing(0, 20)
+        self.hBoxLayout.insertWidget(
+            1, self.iconLabel, 0, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self.window().windowIconChanged.connect(self.setIcon)
+
+        # add title label
+        self.titleLabel = QLabel(self)
+        self.hBoxLayout.insertWidget(
+            2, self.titleLabel, 0, Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
+        self.titleLabel.setObjectName('titleLabel')
+        self.window().windowTitleChanged.connect(self.setTitle)
+
+        self.vBoxLayout = QVBoxLayout()
+        self.buttonLayout = QHBoxLayout()
+        self.buttonLayout.setSpacing(0)
+        self.buttonLayout.setContentsMargins(0, 0, 0, 0)
+        self.buttonLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.buttonLayout.addWidget(self.minBtn)  # This line adds the minBtn
+        self.buttonLayout.addWidget(self.maxBtn)  # This line adds the maxBtn
+        self.buttonLayout.addWidget(self.closeBtn)  # This line adds the closeBtn
+        self.vBoxLayout.addLayout(self.buttonLayout)
+        self.vBoxLayout.addStretch(1)
+        self.hBoxLayout.addLayout(self.vBoxLayout, 0)
+
+    def setTitle(self, title):
+        self.titleLabel.setText(title)
+        self.titleLabel.adjustSize()
+
+    def setIcon(self, icon):
+        self.iconLabel.setPixmap(QIcon(icon).pixmap(20, 20))
+
+    def resizeEvent(self, e):
+        pass
+
+
+class Window(FramelessWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setTitleBar(CustomTitleBar(self))
+
+        # use dark theme mode
+        setTheme(Theme.DARK)
+
+        # change the theme color
+        setThemeColor('#000000')
+
+        self.hBoxLayout = QHBoxLayout(self)
+        self.navigationBar = NavigationBar(self)
+        self.stackWidget = StackedWidget(self)
+
+        # create sub interface
+        self.uiInterface = YoutubeVideo()
+        self.settingsInterface = SettingsPage()
+        # self.artistInterface = artist.Artist(self)
+        # self.libraryInterface = Widget('library Interface', self)
+
+        # initialize layout
+        self.initLayout()
+
+        # add items to navigation interface
+        self.initNavigation()
+
+        self.initWindow()
+
+    def initLayout(self):
+        self.hBoxLayout.setSpacing(0)
+        self.hBoxLayout.setContentsMargins(0, 48, 0, 0)
+        self.hBoxLayout.addWidget(self.navigationBar)
+        self.hBoxLayout.addWidget(self.stackWidget)
+        self.hBoxLayout.setStretchFactor(self.stackWidget, 1)
+
+    def initNavigation(self):
+        self.addSubInterface(self.uiInterface, FIF.VIDEO, 'Video', selectedIcon=FIF.VIDEO)
+        self.addSubInterface(self.settingsInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM,
+                             FIF.SETTING)
+        self.navigationBar.addItem(
+            routeKey='About',
+            icon=FIF.HELP,
+            text='About',
+            onClick=self.showMessageBox,
+            selectable=False,
+            position=NavigationItemPosition.BOTTOM,
+        )
+
+        self.stackWidget.currentChanged.connect(self.onCurrentInterfaceChanged)
+        self.navigationBar.setCurrentItem(self.uiInterface.objectName())
+
+    def initWindow(self):
+        self.resize(1000, 600)
+        self.setWindowIcon(QIcon('resource/icons/icon.png'))
+        self.setWindowTitle('Youtility')
+        self.setQss()
+
+    def addSubInterface(self, interface, icon, text: str, position=NavigationItemPosition.TOP, selectedIcon=None):
+        """ add sub interface """
+        self.stackWidget.addWidget(interface)
+        self.navigationBar.addItem(
+            routeKey=interface.objectName(),
+            icon=icon,
+            text=text,
+            onClick=lambda: self.switchTo(interface),
+            selectedIcon=selectedIcon,
+            position=position,
+        )
+
+    def setQss(self):
+        color = 'dark' if isDarkTheme() else 'light'
+        with open(f'resource/{color}/demo.qss', encoding='utf-8') as f:
+            self.setStyleSheet(f.read())
+
+    def switchTo(self, widget):
+        self.stackWidget.setCurrentWidget(widget)
+
+    def onCurrentInterfaceChanged(self, index):
+        widget = self.stackWidget.widget(index)
+        self.navigationBar.setCurrentItem(widget.objectName())
+
+    def showMessageBox(self):
+        text_for_about = "Heya! it's Rohan, the creator of cvGen. I hope you've enjoyed using this app as much as I enjoyed making it." + "" + "\n" + "\n" \
+                                                                                                                                                      "I'm a school student and I can't earn my own money LEGALLY. So any donations will be largely appreciated. Also, if you find any bugs / have any feature requests, you can open a Issue/ Pull Request in the Repo." \
+                                                                                                                                                      "You can visit GitHub by pressing the button below. You can find Ko-Fi link there :) " + "\n" + "\n" + \
+                         "Once again, thank you for using cvGen. Please consider giving it a star ⭐ as it will largely motivate me to create more of such apps. Also do consider giving me a follow ;) "
+        w = MessageBox(
+            'cvGen',
+            text_for_about,
+            self
+        )
+        w.yesButton.setText('GitHub')
+        w.cancelButton.setText('Return')
+
+        if w.exec():
+            QDesktopServices.openUrl(QUrl("https://github.com/rohankishore/cvGen"))
+
+
+if __name__ == '__main__':
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+
+    # ----- DEPRECIATED ------ #
+
+    # QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    # QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    # ----- DEPRECIATED ------ #
+    qdarktheme.enable_hi_dpi()
+    app = QApplication(sys.argv)
+    w = Window()
+    qdarktheme.setup_theme("dark", custom_colors={"primary": "#c69ef7"})
+    w.show()
+    app.exec()
