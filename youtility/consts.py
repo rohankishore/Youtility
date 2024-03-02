aad8b688fa62efb5aa560d0ec4b60a71062de470
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-03-03 03:01:45 +0530

    constants

---
+++ youtility/consts.py
@@ -0,0 +1,9 @@
+msgs = [
+    "Whoops! It seems the URL field is feeling empty, just like the void of space. Try filling it with a YouTube link!",
+    "Looks like you forgot to give us a ticket to the YouTube show! Please enter a valid URL",
+    "Uh-oh! The YouTube train can't leave the station without a proper URL ticket. Mind providing one?",
+    "It's as quiet as a silent movie in here! Don't forget to drop a YouTube link for us to watch",
+    "This is awkward... We were expecting a YouTube URL, but it seems to have taken a coffee break. Care to join the search?",
+    "Houston, we have a problem! It appears the YouTube coordinates are missing. Please input a valid URL to proceed.", ]
+
+extension = "mp4"
